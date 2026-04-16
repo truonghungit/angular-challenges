c45ee0105fa1f826f99b1228ed42853f4f15e6b5
Hung Pham <truonghungit@gmail.com>
2026-04-16 20:19:07 +0700

    feat(challenge 41): refactor rating-control component to custom form control

--- apps/forms/41-control-value-accessor/src/app/rating-control/rating-control.component.ts
+++ apps/forms/41-control-value-accessor/src/app/rating-control/rating-control.component.ts
@@ -1,22 +1,58 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-rating-control',
   templateUrl: 'rating-control.component.html',
   styleUrls: ['rating-control.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => RatingControlComponent),
+    },
+  ],
 })
-export class RatingControlComponent {
-  @Output()
-  readonly ratingUpdated: EventEmitter<string> = new EventEmitter<string>();
+export class RatingControlComponent implements ControlValueAccessor {
+  protected value: number | null = null;
+  protected disabled = false;
 
-  value: number | null = null;
+  private onChange!: (value: string) => void;
+  private onTouched!: () => void;
 
   setRating(index: number): void {
+    if (this.disabled) {
+      return;
+    }
+
     this.value = index + 1;
-    this.ratingUpdated.emit(`${this.value}`);
+
+    if (this.onTouched && typeof this.onTouched === 'function') {
+      this.onTouched();
+    }
+
+    if (this.onChange && typeof this.onChange === 'function') {
+      this.onChange(`${this.value}`);
+    }
   }
 
   isStarActive(index: number, value: number | null): boolean {
     return value ? index < value : false;
+  }
+
+  writeValue(value: number | null): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
   }
 }
